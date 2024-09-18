c8a86140a1bf316a3dcbd48aa5d9d0855f961fb7
Adarsh Kadam <120251944+adarsh47-bug@users.noreply.github.com>
2024-09-19 01:38:52 +0530

    Update messenger.jsx

--- src/pages/messenger/messenger.jsx
+++ src/pages/messenger/messenger.jsx
@@ -25,7 +25,7 @@ export default function Messenger() {
   const scrollRef = useRef();
 
   useEffect(() => {
-    socket.current = io("ws://localhost:8900");
+    socket.current = io("https://alumnet-socket-proto.onrender.com/");
     socket.current.on("getMessage", (data) => {
       setArrivalMessage({
         sender: data.senderId,
