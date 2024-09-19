6db8a195f6a9d66ad2142e2a31354bcd4351c5ea
Adarsh Kadam <120251944+adarsh47-bug@users.noreply.github.com>
2024-09-19 16:25:47 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -2,8 +2,9 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  // baseURL: 'https://basic-app-backend.azurewebsites.net/',
-  baseURL: 'https://alumnet-backend-proto.onrender.com/',
+  baseURL: 'https://alumnet-proto-backend.azurewebsites.net/',
+  // baseURL: 'https://alumnet-backend-proto.onrender.com/',
+  
   // baseURL: 'http://localhost:5000/',
 });
 
